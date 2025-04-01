7aa65ed12a2cffbf1d944b8342c5c8b53e3ce4c6
Joao <joaoizumi27@gmail.com>
2025-04-01 18:47:02 -0300

    att

---
+++ excecoes/Exceptions.java
@@ -0,0 +1,50 @@
+package excecoes;
+
+public class Exceptions {
+    public static void main(String[] args) {
+        try { //bloco para ser testado para erros enquanto esta sendo executado
+            
+        } catch (Exception e) { //bloco de codigo para ser executado caso um erro ocorra no try
+        }
+
+        try () {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        try {
+            
+        } catch (Exception e) {
+        } finally { // bloco que vai ser executado independentemente se tiver um erro ou nao
+        }
+
+        try {
+            
+        } finally {
+        }
+
+        /*
+         * Nome
+Causa
+java.lang.NullPointerException
+
+Quando tentamos obter alguma informação de uma variável nula.
+
+java.lang.ArithmeticException
+
+Quando tentamos dividir um valor por zero.
+
+java.sql.SQLException
+
+Quando existe algum erro relacionado a interação com banco de dados.
+
+java.io.FileNotFoundException
+
+Quando tentamos ler ou escrever em um arquivo que não existe.
+        */
+
+    }
+}
+
+// 
